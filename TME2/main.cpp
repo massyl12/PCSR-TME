d4c6d7abd4c2985ba4728943f33df4006c982332
fiplox <me@vpatuta.xyz>
2022-09-30 11:07:30 +0200

    question 2

---
+++ TME2/main.cpp
@@ -0,0 +1,48 @@
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <vector>
+
+int main() {
+  using namespace std;
+  using namespace std::chrono;
+
+  ifstream input = ifstream("./WarAndPeace.txt");
+
+  auto start = steady_clock::now();
+  cout << "Parsing War and Peace" << endl;
+
+  size_t nombre_lu = 0;
+  // prochain mot lu
+  string word;
+  // une regex qui reconnait les caractères anormaux (négation des lettres)
+  regex re(R"([^a-zA-Z])");
+  vector<string> words;
+  while (input >> word) {
+    // élimine la ponctuation et les caractères spéciaux
+    word = regex_replace(word, re, "");
+    // passe en lowercase
+    transform(word.begin(), word.end(), word.begin(), ::tolower);
+
+    if (std::find(words.begin(), words.end(), word) == words.end())
+      words.push_back(word);
+    // word est maintenant "tout propre"
+    // if (nombre_lu % 100 == 0)
+    //   // on affiche un mot "propre" sur 100
+    //   cout << nombre_lu << ": " << word << endl;
+    nombre_lu++;
+  }
+  input.close();
+
+  cout << "Finished Parsing War and Peace" << endl;
+
+  auto end = steady_clock::now();
+  cout << "Parsing took " << duration_cast<milliseconds>(end - start).count()
+       << "ms.\n";
+
+  cout << "Found a total of " << nombre_lu << " words.\n";
+  cout << "Found a total of " << words.size() << " different words." << endl;
+
+  return 0;
+}
