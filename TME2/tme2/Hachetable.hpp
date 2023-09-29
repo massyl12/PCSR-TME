e7bf73a2a6bb598f92db742d56529fe6dacb5193
28706508 <28706508@ppti-14-401-12.ufr-info-p6.jussieu.fr>
2023-09-29 11:38:33 +0200

    New Tme2

---
+++ TME2/tme2/Hachetable.hpp
@@ -0,0 +1,16 @@
+// Defines the HashTable item.
+
+typedef struct Ht_item
+{
+    char* key;
+    char* value;
+} Ht_item;
+
+// Defines the HashTable.
+typedef struct HashTable
+{
+    // Contains an array of pointers to items.
+    Ht_item** items;
+    int size;
+    int count;
+} HashTable;
