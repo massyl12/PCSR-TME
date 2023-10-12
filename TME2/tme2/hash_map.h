38ef77c82e4b63e18eddea9cae054f3fcf1ccf9c
Millouze <74658025+Millouze@users.noreply.github.com>
2023-10-12 11:30:03 +0200

    Vectorise fun added for Q7
    Does not use template types.

--- TME2/tme2/hash_map.h
+++ TME2/tme2/hash_map.h
@@ -2,11 +2,18 @@
 #pragma once
 #include <vector>
 #include <forward_list>
+#include <utility>
+
 namespace pr {
 
     template <typename K, typename V>
 
     class HashTable {
+
+        
+            
+            
+
 
         public: 
             class Entry {
@@ -17,63 +24,41 @@ namespace pr {
                     Entry (K key, V val):key(key), val(val){}
             };
 
-            std::vector<std::forward_list<Entry>> vec;
+            std::vector<std::forward_list<Entry>> bucket;
+            size_t size_;
 
         
             HashTable (int size) {
-                vec.reserve(size);
+                bucket.reserve(size);
+                for(size_t i = 0;i<size;i++){
+                    bucket.push_back(std::forward_list<Entry>());
+                }
             }
+            ~HashTable () {
+                for (int i = 0; i < bucket.size(); ++i) {
+                    bucket[i].clear();
+                }
+                bucket.clear();
+            }
+
 
             V * get (const K & key){
                 size_t h = std::hash<K>()(key);
-                size_t target = h % vec.capacity();
-                std::forward_list<Entry> bucket = vec[target];
-                for (Entry &ent : vec[target]){
+                size_t target = h % bucket.capacity();
+                
+                for (Entry &ent : bucket[target]){
                     if (ent.key  == key) {
                         return &ent.val;
                     }
                 }
 
-                /* for (auto it = bucket.begin(); it != bucket.end(); ++it) {
-                    if (it->key == key) {
-                        return &(it->val);
-                    }
-                } */
+                
                 return nullptr; //key not found
             }
 
-            ~HashTable () {
-                for (int i = 0; i < vec.size(); ++i) {
-                    vec[i].clear();
-                }
-                vec.clear();
-            }
+           
 
-            void grow () {
-                //On alloue un nouveau vecteur avec le double de la taille actuelle
-                std::vector<std::forward_list<Entry>> new_vec (vec.size()*2, std::forward_list<Entry>());
-
-                //On ajoute dans les nouveau vecteur les éléments présents qui étaient déjà présents avant
-                Entry * ent;
-                for (int i = 0; i < vec.size(); ++i){
-                    for (Entry & enti : vec[i]){
-
-                        //On calcule le nouvel index (car la taille a changé)
-                        size_t h = std::hash<K>()(enti.key);
-                        size_t target = h % new_vec.size();
-                        std::forward_list<Entry> bucket = vec[target];
-
-                        ent = new Entry(enti.key, enti.val);
-                        bucket.push_front(ent);
-                    }
-                } 
-
-                //On supprime l'ancien vecteur
-                ~HashTable<K,V>();
-
-                vec = new_vec;
-            } 
-
+            
             bool put (const K & key, const V & value) {
                 
                 //Mise à jour d'une valeur déjà existante
@@ -87,15 +72,28 @@ namespace pr {
 
                 //Réalisation d'une insertion    
                 size_t h = std::hash<K>()(key);
-                size_t target = h % vec.size();
-                std::forward_list<Entry> bucket = vec[target];
+                size_t target = h % bucket.size();
+                size_++;
 
-                bucket.push_front(Entry(key, value));
+                bucket[target].push_front(Entry(key, value));
                 return false;
             }
 
             size_t size () const {
-                return vec.size();
+                return size_;
+            }
+
+            std::vector<std::pair<std::string,int>>& vectorize(std::vector<std::pair<std::string,int>>& vec){
+                
+                int capa = bucket.capacity();
+                for(int i = 0 ; i <capa ;i++){
+                    if(bucket[i].empty==false){
+                        for(auto it = bucket[i].begin(); it<bucket[i].end();it++ ){
+                            vec.push_back(std::pair<std::string,int>(it->key,it->val));
+                        }
+                    }
+                }
+                return vec;
             }
 
     };
