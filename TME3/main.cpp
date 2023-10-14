351bad33e79d82159edbc02764b78ed8f640186c
Malek Bouzarkoua <MalekBouzarkoua58@gmail.com>
2023-10-14 21:09:09 +0200

    3 questions of tme3

--- TME3/main.cpp
+++ TME3/main.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
-#include "..\include\String.hpp"
+#include <vector>
+#include <string>
+
+namespace pr {
+    template<typename Iterator>
+    size_t count(Iterator begin, Iterator end) {
+        size_t cpt = 0;
+        while (begin != end) {
+            ++begin;
+            ++cpt;
+        }
+        return cpt;
+    }
+
+    template<typename Iterator, typename T>
+    size_t count_if_equal(Iterator begin, Iterator end, const T &val) {
+        size_t cpt = 0;
+        while (begin != end) {
+            if (*begin == val)  
+                ++cpt;
+            ++begin;
+        }
+        return cpt;
+    }
+}
 
 int main() {
-    pr::String x("Le penseur");
-    std::cout << "La taille de la chaine est " <<  x.length() << "\n";
+    std::vector<std::string> VectString;  
+    VectString.push_back("un");
+    VectString.push_back("deux");
+    VectString.push_back("trois");
+    VectString.push_back("un");
 
-    pr::String x2;
-    x2.newcopy("oui"); // Utilisez la fonction newcopy pour créer une nouvelle chaîne
-
+    std::cout <<pr::count(VectString.begin(),VectString.end()) << std::endl;
+    std::cout <<pr::count_if_equal(VectString.begin(),VectString.end(),"un") << std::endl;
     return 0;
 }
