7a3dd990d54e57186f6aa8cebe71eadf3aa97169
LorieXue <28705252@ppti-14-401-09.ufr-info-p6.jussieu.fr>
2023-11-24 16:11:34 +0100

    TME 7: semaphore, memoire partage

--- TME7/src/Stack.h
+++ TME7/src/Stack.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include <cstring> // size_t,memset
+#include <semaphore.h>
+#include <ostream>
+#include <iostream>
 
 namespace pr {
+using namespace std;
 
 #define STACKSIZE 100
 
@@ -10,18 +14,37 @@ template<typename T>
 class Stack {
 	T tab [STACKSIZE];
 	size_t sz;
+	sem_t mutex, s_cons, s_prod;
 public :
-	Stack () : sz(0) { memset(tab,0,sizeof tab) ;}
+	Stack () : sz(0) { memset(tab,0,sizeof tab) ;
+		sem_init(&mutex, 1, 1);
+		sem_init(&s_cons, 1, 0);
+		sem_init(&s_prod, 1, STACKSIZE);
+	}
 
 	T pop () {
-		// bloquer si vide
+		cout << "pop" << endl;
+		sem_wait(&s_cons);
+		sem_wait(&mutex);
 		T toret = tab[--sz];
+		sem_post(&mutex);
+		sem_post(&s_prod);
 		return toret;
 	}
 
 	void push(T elt) {
-		//bloquer si plein
+		cout << "push " << elt << endl;
+		sem_wait(&s_prod);
+		sem_wait(&mutex);
 		tab[sz++] = elt;
+		sem_post(&mutex);
+		sem_post(&s_cons);
+	}
+
+	~Stack (){
+		sem_destroy(&mutex);
+		sem_destroy(&s_cons);
+		sem_destroy(&s_prod);
 	}
 };
 
--- TME7/src/prod_cons.cpp
+++ TME7/src/prod_cons.cpp
@@ -1,39 +1,57 @@
 #include "Stack.h"
 #include <iostream>
 #include <unistd.h>
+#include <signal.h>
 #include <sys/wait.h>
+#include <sys/mman.h>
 #include <vector>
 
 
 using namespace std;
 using namespace pr;
 
-#define N 5
-#define M 6
-
+#define N 1
+#define M 2
+bool b = true;
 
 void producteur (Stack<char> * stack) {
 	char c ;
-	while (cin.get(c)) {
+	while (cin.get(c) && b) {
+		cout << "prod " << c << endl;
 		stack->push(c);
+		cout << "prod end" << endl;
 	}
 }
 
 void consomateur (Stack<char> * stack) {
-	while (true) {
+	while (b) {
+		cout << "cons" <<endl;
 		char c = stack->pop();
 		cout << c << flush ;
 	}
 }
 
-int main () {
-	Stack<char> * s = new Stack<char>();
 
+int main () {
+	void* ptr = mmap(NULL, sizeof(Stack<char>),PROT_WRITE|PROT_READ,MAP_SHARED|MAP_ANONYMOUS,-1,0);
+	if (ptr == MAP_FAILED) {
+	    // Handle the error
+	    perror("mmap");
+	    exit(EXIT_FAILURE);
+	}
+	Stack<char> * s = new (ptr) Stack<char>();
+	signal(SIGINT,[](int sig){
+		b=false;
+	});
+	vector<pid_t> sons;
+	sons.reserve(N+M);
 	for(int i=0;i<N;i++){
 		pid_t pp = fork();
 		if (pp==0) {
 			producteur(s);
 			return 0;
+		} else {
+			sons.push_back(pp);
 		}
 	}
 
@@ -42,12 +60,20 @@ int main () {
 		if (pc==0) {
 			consomateur(s);
 			return 0;
+		}else {
+			sons.push_back(pc);
 		}
 	}
+
+	while(b){}
+
+	for(int i=0;i<N+M;i++)
+		kill(sons[i],SIGINT);
 
 	for(int i=0;i<N+M;i++)
 		wait(0);
 
+	munmap(ptr, sizeof(Stack<char>));
 	delete s;
 	return 0;
 }
