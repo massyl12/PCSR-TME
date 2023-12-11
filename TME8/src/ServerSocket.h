f7134d61a0e416aaf42739664cdf5feb461daaf7
LorieXue <28705252@ppti-14-302-02.ufr-info-p6.jussieu.fr>
2023-12-11 14:33:39 +0100

    TME8: Q1

--- TME8/src/ServerSocket.h
+++ TME8/src/ServerSocket.h
@@ -2,6 +2,8 @@
 #define SRC_SERVERSOCKET_H_
 
 #include "Socket.h"
+#include <sys/socket.h>
+#include <iostream>
 
 namespace pr {
 
@@ -20,5 +22,36 @@ public :
 	void close();
 };
 
+ServerSocket::ServerSocket(int port){
+	struct sockaddr_in sin;
+	sin.sin_family = AF_UNSPEC;
+	sin.sin_port = htons(port);
+	sin.sin_addr.s_addr = INADDR_ANY;
+	int fd=socket(AF_UNSPEC, SOCK_STREAM, 0);
+	if(!fd){
+		perror("socket error");
+		exit(-1);
+	}
+
+	if(bind(fd, (struct sockaddr*) &sin, sizeof(sin))){
+		::close(fd);
+	}
+	listen(fd,5);
+	socketfd = fd;
+}
+
+Socket ServerSocket::accept(){
+	int clientfd = ::accept(socketfd,0,0);
+	struct sockaddr_in addr;
+	socklen_t len = sizeof(addr);
+	if((clientfd=::accept(socketfd,(struct sockaddr*) &addr,&len))){
+		perror("accept socket error");
+		exit(-1);
+	}
+	std::cout << "Connexion de " << &addr << std::endl;
+	return clientfd;
+
+}
 } // ns pr
 #endif /* SRC_SERVERSOCKET_H_ */
+
--- TME8/src/Socket.h
+++ TME8/src/Socket.h
@@ -4,6 +4,13 @@
 #include <netinet/ip.h>
 #include <string>
 #include <iosfwd>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 
 namespace pr {
 
@@ -16,7 +23,8 @@ public :
 
 	// tente de se connecter à l'hôte fourni
 	void connect(const std::string & host, int port);
-	void connect(in_addr ipv4, int port);
+	//void connect(in_addr ipv4, int port);
+	void connect(sockaddr addr, socklen_t addr_len);
 
 	bool isOpen() const {return fd != -1;}
 	int getFD() { return fd ;}
@@ -24,7 +32,54 @@ public :
 	void close();
 };
 
-std::ostream & operator<< (std::ostream & os, struct sockaddr_in * addr);
+std::ostream & operator<< (std::ostream & os, struct sockaddr_in * addr){
+	char hname[1024];
+	if(!getnameinfo((struct sockaddr *) addr, sizeof(*addr),hname,1024,nullptr,0,0)){
+		os << std::string(hname);
+	}
+	os << std::string(inet_ntoa(addr->sin_addr)) << std::string(".") << std::to_string(ntohs(addr->sin_port));
+	return os;
+}
+
+
+void Socket::connect(const std::string & host, int port){
+	struct addrinfo* res, *rp;
+	int s = getaddrinfo(host.c_str(),std::to_string(port).c_str(),AF_UNSPEC,&res);
+	if (s != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+		exit(EXIT_FAILURE);
+	}
+	for (rp = res; rp != NULL; rp = rp->ai_next) {
+		fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+		if (fd == -1)
+			continue;
+
+		if (::connect(fd, rp->ai_addr, rp->ai_addrlen) != -1)
+			break; /* Success */
+
+		::close(fd);
+	}
+
+	freeaddrinfo(res); /* No longer needed */
+}
+
+void Socket::connect(sockaddr addr, socklen_t addr_len){
+	fd = socket(AF_UNSPEC, SOCK_STREAM, 0);
+	if(!fd){
+		perror("socket error");
+		exit(-1);
+	}
+	if(::connect(fd,&addr, addr_len)){
+		::close(fd);
+		perror("connection error");
+		exit(-2);
+	}
+}
+
+void Socket::close(){
+	shutdown(fd,SHUT_RDWR);
+	::close(fd);
+}
 
 }
 
